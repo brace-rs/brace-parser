2ad0d4b47818eb0a394c572e0e92271942a66c3b
Daniel Balcomb <daniel.balcomb@gmail.com>
2019-10-22 14:10:09 +0100

    Add expected error variant

--- crates/brace-parser/src/error.rs
+++ crates/brace-parser/src/error.rs
@@ -1,10 +1,14 @@
 use std::error;
 use std::fmt;
 
+use crate::character::Character;
+use crate::sequence::Sequence;
+
 #[derive(Clone, Debug, PartialEq)]
 pub enum Error {
     Incomplete,
     Unexpected(char),
+    Expected(Expect),
 }
 
 impl Error {
@@ -24,6 +28,22 @@ impl fmt::Display for Error {
         match self {
             Self::Incomplete => write!(f, "Error: Unexpected end of input"),
             Self::Unexpected(ch) => write!(f, "Error: Unexpected character: '{}'", ch),
+            Self::Expected(expect) => write!(f, "Error: Expected {}", expect),
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum Expect {
+    Character(Character),
+    Sequence(Sequence),
+}
+
+impl fmt::Display for Expect {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::Character(ch) => write!(f, "character: {}", ch),
+            Self::Sequence(seq) => write!(f, "sequence: {}", seq),
         }
     }
 }
