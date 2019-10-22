324cb87683fa13c8e1d369f479ddfc393519240d
Daniel Balcomb <daniel.balcomb@gmail.com>
2019-10-22 21:51:20 +0100

    Add map combinator

--- crates/brace-parser/src/combinator.rs
+++ crates/brace-parser/src/combinator.rs
@@ -48,6 +48,13 @@ pub fn trailing<'a, O, T>(
     }
 }
 
+pub fn map<'a, M, A, B>(parser: impl Parser<'a, A>, map: M) -> impl Parser<'a, B>
+where
+    M: Fn(A) -> B,
+{
+    move |input| parser.parse(input).map(|(out, rem)| (map(out), rem))
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -219,6 +226,18 @@ mod tests {
         assert_eq!(
             parse("hello universe!", trailing("hello world", "!")),
             Err(Error::expect('w').but_found('u'))
+        );
+    }
+
+    #[test]
+    fn test_map() {
+        assert_eq!(
+            parse("hello", map("hello", |seq| seq.to_owned() + "!")),
+            Ok(("hello!".to_owned(), ""))
+        );
+        assert_eq!(
+            parse("hello", map('h', Option::Some)),
+            Ok((Some('h'), "ello"))
         );
     }
 }
