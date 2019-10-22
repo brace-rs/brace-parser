e23fa216a174a49896aa4371d9617707bbf59a90
Daniel Balcomb <daniel.balcomb@gmail.com>
2019-10-22 14:12:13 +0100

    Add expected error variant constructor

--- crates/brace-parser/src/error.rs
+++ crates/brace-parser/src/error.rs
@@ -19,6 +19,13 @@ impl Error {
     pub fn unexpected(ch: char) -> Self {
         Self::Unexpected(ch)
     }
+
+    pub fn expected<T>(expect: T) -> Self
+    where
+        T: Into<Expect>,
+    {
+        Self::Expected(expect.into())
+    }
 }
 
 impl error::Error for Error {}
@@ -45,5 +52,35 @@ impl fmt::Display for Expect {
             Self::Character(ch) => write!(f, "character: {}", ch),
             Self::Sequence(seq) => write!(f, "sequence: {}", seq),
         }
+    }
+}
+
+impl From<char> for Expect {
+    fn from(from: char) -> Self {
+        Self::Character(Character::custom(from))
+    }
+}
+
+impl From<Character> for Expect {
+    fn from(from: Character) -> Self {
+        Self::Character(from)
+    }
+}
+
+impl From<&str> for Expect {
+    fn from(from: &str) -> Self {
+        Self::Sequence(Sequence::custom(from))
+    }
+}
+
+impl From<String> for Expect {
+    fn from(from: String) -> Self {
+        Self::Sequence(Sequence::custom(from))
+    }
+}
+
+impl From<Sequence> for Expect {
+    fn from(from: Sequence) -> Self {
+        Self::Sequence(from)
     }
 }
