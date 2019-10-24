33018edef67c0fe17cb3b88bcd398109745ec238
Daniel Balcomb <daniel.balcomb@gmail.com>
2019-10-24 21:25:34 +0100

    Add map error combinator

--- crates/brace-parser/src/combinator/mod.rs
+++ crates/brace-parser/src/combinator/mod.rs
@@ -1,3 +1,4 @@
+use crate::error::Error;
 use crate::parser::Parser;
 
 pub mod branch;
@@ -8,6 +9,13 @@ where
     M: Fn(A) -> B,
 {
     move |input| parser.parse(input).map(|(out, rem)| (map(out), rem))
+}
+
+pub fn map_err<'a, O, M>(parser: impl Parser<'a, O>, map: M) -> impl Parser<'a, O>
+where
+    M: Fn(Error) -> Error,
+{
+    move |input| parser.parse(input).map_err(|err| map(err))
 }
 
 #[cfg(test)]
@@ -24,6 +32,18 @@ mod tests {
         assert_eq!(
             parse("hello", map('h', Option::Some)),
             Ok((Some('h'), "ello"))
+        );
+    }
+
+    #[test]
+    fn test_map_err() {
+        assert_eq!(
+            parse("", map_err("hello", |_| Error::expect('!'))),
+            Err(Error::expect('!'))
+        );
+        assert_eq!(
+            parse("h", map_err("hello", |err| err.but_found('!'))),
+            Err(Error::expect('e').but_found('!'))
         );
     }
 }
