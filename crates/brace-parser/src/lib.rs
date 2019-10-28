ae97af44b1e5d9f74ba5e8c070b65fd225ebcc48
Daniel Balcomb <daniel.balcomb@gmail.com>
2019-10-28 17:00:16 +0000

    Add unescape combinator (#35)

--- crates/brace-parser/src/lib.rs
+++ crates/brace-parser/src/lib.rs
@@ -7,7 +7,7 @@ pub mod sequence;
 pub mod prelude {
     pub use crate::combinator::branch::{branch, either, optional};
     pub use crate::combinator::series::{delimited, leading, list, pair, series, trailing, trio};
-    pub use crate::combinator::{consume, context, escaped, map, map_err, not};
+    pub use crate::combinator::{consume, context, escaped, map, map_err, not, unescape};
     pub use crate::error::{Error, Expect};
     pub use crate::parser::{parse, take, take_while, Output, Parser};
     pub use crate::{character, sequence};
