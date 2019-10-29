ca45ed455c64c5f36007d366984cef99b26a8c93
Daniel Balcomb <daniel.balcomb@gmail.com>
2019-10-29 23:55:12 +0000

    Export pass and fail error combinators in prelude (#38)

--- crates/brace-parser/src/lib.rs
+++ crates/brace-parser/src/lib.rs
@@ -7,7 +7,9 @@ pub mod sequence;
 pub mod prelude {
     pub use crate::combinator::branch::{branch, either, optional};
     pub use crate::combinator::series::{delimited, leading, list, pair, series, trailing, trio};
-    pub use crate::combinator::{consume, context, escaped, map, map_err, not, unescape};
+    pub use crate::combinator::{
+        consume, context, escaped, fail, map, map_err, not, pass, unescape,
+    };
     pub use crate::error::{Error, Expect};
     pub use crate::parser::{parse, take, take_while, Output, Parser};
     pub use crate::{character, sequence};
